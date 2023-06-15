d7679600a76fb758f4a5879919266cbbf52284fd
Drake Loch <tehtrojenguy@gmail.com>
2023-06-14 23:55:41 -0600

    pushing work

--- src/components/common/PageLayout/PageLayout.svelte
+++ src/components/common/PageLayout/PageLayout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 </script>
 
-<section class="flex gap-2 w-full">
+<section class="flex gap-6 w-full">
 	<slot />
 </section>
--- src/routes/view/+page.svelte
+++ src/routes/view/+page.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
+	import Article from '../../components/common/Article/Article.svelte';
 	import PageLayout from '../../components/common/PageLayout/PageLayout.svelte';
 	import Toolbar from '../../components/toolbar/Toolbar.svelte';
 </script>
 
 <PageLayout>
 	<Toolbar />
-	<div class="w-full border border-black">
-		<h1>title</h1>
-	</div>
+	<Article />
 </PageLayout>
