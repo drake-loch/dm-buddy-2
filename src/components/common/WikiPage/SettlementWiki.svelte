9f66f1b136d3c6c7ae5b31c286e61f15c614d2b7
Drake Loch <tehtrojenguy@gmail.com>
2023-07-11 13:00:29 -0600

    code refactoring

--- src/components/common/WikiPage/SettlementWiki.svelte
+++ src/components/common/WikiPage/SettlementWiki.svelte
@@ -16,26 +16,23 @@
 	import WikiMultiSelect from './components/WikiMultiSelect.svelte';
 	import WikiPanelKeyValue from './components/WikiPanelKeyValue.svelte';
 	import WikiPanelSection from './components/WikiPanelSection.svelte';
+	import WikiPanelSelect from './components/WikiPanelSelect.svelte';
+	import WikiPanelTitle from './components/WikiPanelTitle.svelte';
 
 	export let editMode = false;
 	export let settlement: Settlement = newSettlement();
 	export let save: (settlement: Settlement) => number = () => saveSettlement(settlement);
 
-	const people = getNPCsAndChars().map((c) => {
+	const people = getNPCs().map((c) => {
 		return {
-			value: { id: c.id, type: c.type, role: '', name: c.fullName },
+			value: { id: c.id, type: c.type },
 			label: c.fullName,
-			type: c.type,
 			action: () => {
 				//view character
 				// toggleMod('npc', c.id);
 			}
 		};
 	});
-
-	$: console.log(people);
-
-	let authFigures: Settlement['authorityFigures'] = settlement.authorityFigures ?? [];
 </script>
 
 <WikiPage
@@ -65,18 +62,7 @@
 			<WikiPanelKeyValue {editMode} label="Population:" bind:value={settlement.population} />
 			<WikiPanelKeyValue {editMode} label="Alignment:" bind:value={settlement.alignment} />
 			<WikiPanelKeyValue {editMode} label="Government:" bind:value={settlement.government} />
-			<Select
-				{editMode}
-				label="Authority Figures:"
-				bind:values={settlement.authorityFigures}
-				options={people ?? []}
-			/>
-			<div class="flex gap-4 items-center justify-between my-4 border border-black py-2 px-1">
-				{#each settlement.authorityFigures as authFigure}
-					<label for="role">{people.find((p) => p.value === authFigure)?.label}</label>
-					<WikiPanelKeyValue {editMode} label="Role:" bind:value={authFigure.role} />
-				{/each}
-			</div>
+
 			<WikiPanelKeyValue {editMode} label="Economy:" bind:value={settlement.economy} />
 			<WikiPanelKeyValue {editMode} label="Languages:" bind:value={settlement.languages} />
 			<WikiMultiSelect
@@ -85,6 +71,98 @@
 				bind:values={settlement.organizations}
 				options={[]}
 			/>
+		</WikiPanelSection>
+
+		<WikiPanelSection>
+			<WikiPanelTitle title="Authority Figures" />
+			<div class="flex flex-col gap-2 items-center">
+				{#each settlement.authorityFigures as authFigure, i}
+					<div class="flex w-full items-center gap-2">
+						{#if editMode}
+							{#if authFigure.isLinked}
+								<span class="w-1/3">
+									<WikiPanelSelect
+										label="Person:"
+										bind:value={authFigure}
+										options={people}
+										{editMode}
+									/>
+								</span>
+							{:else}
+								<span class="w-1/3">
+									<WikiPanelKeyValue
+										label="Person:"
+										{editMode}
+										bind:value={authFigure.name}
+										valueClass="text-black font-semibold"
+									/>
+								</span>
+							{/if}
+							<span class="w-1/3">
+								<WikiPanelKeyValue
+									label="Role:"
+									{editMode}
+									bind:value={authFigure.role}
+									valueClass="text-black font-semibold"
+								/>
+							</span>
+						{:else}
+							{#if authFigure.role}
+								<label
+									for={`${authFigure.id}-${authFigure.type}-label`}
+									class="text-black font-semibold text-sm"
+									>{authFigure.role}:
+								</label>
+							{/if}
+							{#if authFigure.isLinked}
+								<a href={`/npcs/${authFigure.id}`} class="text-blue-400 text-sm underline"
+									>{people.find((p) => p.value.id === authFigure.id)?.label}</a
+								>
+							{:else}
+								<span class="text-white text-sm">{authFigure.name}</span>
+							{/if}
+						{/if}
+
+						{#if editMode}
+							<input
+								type="checkbox"
+								name={`toggled-${i}`}
+								id=""
+								checked={authFigure.isLinked}
+								on:change={(e) => {
+									// @ts-ignore
+									authFigure.isLinked = e.target.checked;
+								}}
+							/>
+							<button
+								type="button"
+								class="bg-red-600 w-6 h-6"
+								on:click={() => {
+									settlement.authorityFigures = settlement.authorityFigures.filter(
+										(a) => a !== authFigure
+									);
+									settlement = settlement;
+								}}>🗑️</button
+							>
+						{/if}
+					</div>
+				{/each}
+			</div>
+			<div class="flex items-center w-full mt-2">
+				{#if editMode}
+					<button
+						type="button"
+						class="bg-green-600 w-4 h-4 rounded-full flex justify-center items-center pb-1"
+						on:click={() => {
+							settlement.authorityFigures = [
+								...settlement.authorityFigures,
+								{ id: -1, type: 'npc', role: '', isLinked: false }
+							];
+							settlement = settlement;
+						}}>+</button
+					>
+				{/if}
+			</div>
 		</WikiPanelSection>
 
 		<!-- Description -->
